ed0b9da9ab4f7818938da93700b21534d1a38a8f
Anant <anantm95@gmail.com>
2015-02-23 16:33:37 +0530

    home activity changes.

--- app/src/main/java/com/academy/semtrac/HomeActivity.java
+++ app/src/main/java/com/academy/semtrac/HomeActivity.java
@@ -4,14 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 
 public class HomeActivity extends ActionBarActivity {
+
+    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        layout = (LinearLayout) findViewById(R.id.home_layout);
+
     }
 
 
